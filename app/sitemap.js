24e52ca52237f198fcbe4d31fe69be59b027c3f6
masum1234-gok <mirmohammadmasumbillah@gmail.com>
2026-08-02 09:45:04 +0600

    update

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://leakreels.site',
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 1,
+        },
+    ];
+}
