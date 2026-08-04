a7c671ff3523171f14574c0072f560baa17de8c4
masum1234-gok <mirmohammadmasumbillah@gmail.com>
2026-08-04 21:20:12 +0600

    update

--- components/ads/GlobalAdScripts.js
+++ components/ads/GlobalAdScripts.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Script from "next/script";
-import { SOCIAL_BAR_SRC } from "@/lib/ads";
+// import { POPUNDER_SRC, SOCIAL_BAR_SRC } from "@/lib/ads";
 
 // Site-wide ad scripts that only need to load once, anywhere on the page.
 // Mounted in the root layout.
@@ -9,7 +9,7 @@ export default function GlobalAdScripts() {
   return (
     <>
       {/* <Script src={POPUNDER_SRC} strategy="lazyOnload" /> */}
-      <Script src={SOCIAL_BAR_SRC} strategy="lazyOnload" />
+      {/* <Script src={SOCIAL_BAR_SRC} strategy="lazyOnload" /> */}
     </>
   );
 }
